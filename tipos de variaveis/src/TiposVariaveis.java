e0fefd3aab25142cd78330804b5b285ab9e70412
oblytoid <cauangabrielbr@gmail.com>
2023-08-31 12:22:56 -0300

    arrumação de pastas

---
+++ tipos de variaveis/src/TiposVariaveis.java
@@ -0,0 +1,22 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //tipos de variaveis na linguagem java
+
+        //primitivos int, byte , short ,long , float , double , boolean e char
+        // esses tipo de  variaveis não são consideradas objetos
+        // são parecido com a linguagem C/C++
+
+
+        // variaveis constantes devem serem escrita em caixa alta
+        long cachorro =  12121313L;//variaveis long, sempre devem ter um 'l' no final
+        float pi = 3.14f;//variaveis float, sempre devem ter 'f' em seu final
+        System.out.println(pi);// ex
+        System.out.println(cachorro);//ex 2
+
+        //final para 'transformar' uma variavel em uma constante
+        //sempre declarar uma variavel constante em caixa altas
+
+    }
+}
